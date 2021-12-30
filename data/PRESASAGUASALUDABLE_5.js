2afef0daf81f811e0546d00e81f18c9702d3c771
sicnetdgo <96886004+sicnetdgo@users.noreply.github.com>
2021-12-30 16:28:21 -0600

    Add files via upload

---
+++ data/PRESASAGUASALUDABLE_5.js
@@ -0,0 +1,8 @@
+var json_PRESASAGUASALUDABLE_5 = {
+"type": "FeatureCollection",
+"name": "PRESASAGUASALUDABLE_5",
+"features": [
+{ "type": "Feature", "properties": { "NomOficial": "Francisco Zarco", "NomComun": "Las Tórtolas", "ID_Estado": 10.0, "NomEstado": "Durango", "Cuenca": "Cuencas Centrales del Norte", "Corriente": "Río Nazas", "A_Cortina": 40.0, "Cap_Max": 383.0, "Cap_Nom": 309.0, "VolAlmacen": 268.91, "Web": "https://www.google.com/search?q=presa%20Francisco%20Zarco" }, "geometry": { "type": "Point", "coordinates": [ -103.77335968947007, 25.270698560088181 ] } },
+{ "type": "Feature", "properties": { "NomOficial": "Lázaro Cárdenas", "NomComun": "El Palmito", "ID_Estado": 10.0, "NomEstado": "Durango", "Cuenca": "Cuencas Centrales del Norte", "Corriente": "Río Nazas", "A_Cortina": 100.0, "Cap_Max": 4279.0, "Cap_Nom": 2820.0, "VolAlmacen": 1664.53, "Web": "https://www.google.com/search?q=presa%20Lázaro%20Cárdenas" }, "geometry": { "type": "Point", "coordinates": [ -105.01593238716606, 25.59427521132972 ] } }
+]
+}
